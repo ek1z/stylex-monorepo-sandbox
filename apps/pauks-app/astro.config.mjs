09d73731f41c892bb786cd9e02f3b0dcd0bc6d60
Erkki Heikkonen <erkki.heikkonen@luckyfew.fi>
2025-09-01 16:24:09 +0300

    toolchian says BOOM

--- apps/pauks-app/astro.config.mjs
+++ apps/pauks-app/astro.config.mjs
@@ -1,51 +1,69 @@
 import { dirname, resolve } from 'node:path';
 import { fileURLToPath } from 'node:url';
 import react from '@astrojs/react';
+import stylex from '@stylexjs/postcss-plugin';
 import { defineConfig } from 'astro/config';
+import babel from 'vite-plugin-babel';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const libraries = ['@proot/components', '@proot/tokens'];
+const stylexDependencies = ['@proot/components', '@proot/tokens', '@stylexjs/open-props'];
+
+const babelConfig = {
+  babelrc: false,
+  configFile: false,
+  presets: ['@babel/preset-typescript'],
+  plugins: [
+    [
+      '@stylexjs/babel-plugin',
+      {
+        dev: process.env.NODE_ENV !== 'production',
+        test: process.env.NODE_ENV === 'test',
+        runtimeInjection: process.env.NODE_ENV !== 'production',
+        genConditionalClasses: true,
+        treeshakeCompensation: true,
+        aliases: {
+          '@proot/tokens/tokens.stylex': resolve(__dirname, '../../packages/tokens/dist/tokens.stylex.js'),
+        },
+        unstable_moduleResolution: {
+          type: 'commonJS',
+        },
+      },
+    ],
+  ],
+};
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    optimizeDeps: {
-      exclude: libraries,
-    },
     ssr: {
-      optimizeDeps: {
-        exclude: libraries,
-      },
-      noExternal: libraries,
+      // Make sure to make Vite apply the babel transformation also in SSR
+      noExternal: stylexDependencies,
     },
     css: {
       transformer: 'postcss',
-      postcss: resolve(resolve(), 'postcss.config.mjs'),
-    },
-  },
-  integrations: [
-    react({
-      babel: {
-        babelrc: false,
+      postcss: {
         plugins: [
-          [
-            '@stylexjs/babel-plugin',
-            {
-              dev: process.env.NODE_ENV !== 'production',
-              test: process.env.NODE_ENV === 'test',
-              runtimeInjection: process.env.NODE_ENV !== 'production',
-              aliases: {
-                '@proot/tokens/tokens.stylex': resolve(__dirname, '../../packages/tokens/dist/tokens.stylex.js'),
-              },
-              unstable_moduleResolution: {
-                type: 'commonJS',
-              },
-            },
-          ],
+          stylex({
+            babelConfig,
+            useCSSLayers: false,
+            include: [
+              './src/**/*.{ts,tsx,astro}',
+              // Local monorepo packages
+              ...stylexDependencies.map((dep) => resolve(__dirname, `node_modules/${dep}/**/*.{js,jsx,ts,tsx}`)),
+            ],
+          }),
         ],
       },
-    }),
-  ],
+    },
+    plugins: [
+      babel({
+        babelConfig,
+        loader: 'jsx',
+        filter: /\.[jt]sx?$/u,
+      }),
+    ],
+  },
+  integrations: [react()],
 });
